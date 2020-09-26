ca95500f6528963b03b38fa7368a3b3419376caf
benjr70 <benrolf70@gmail.com>
2020-09-26 00:03:11 -0400

    first set up done

--- Movie-Matching/src/app/app.component.ts
+++ Movie-Matching/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
+export class AppComponent  implements OnInit, OnDestroy {
   title = 'Movie-Matching';
+  bodyTag: HTMLBodyElement = document.getElementsByTagName('body')[0];
+  htmlTag: HTMLElement = document.getElementsByTagName('html')[0];
+
+  ngOnInit() {
+    // add the css-style to the html and body tags
+    this.bodyTag.classList.add('home-page');
+    this.htmlTag.classList.add('home-page');
+  }
+
+  ngOnDestroy() {
+    // remove the the body classes
+    this.bodyTag.classList.remove('home-page');
+    this.htmlTag.classList.remove('home-page');
+  }
+
 }
--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 a tinder style app for movie selection for couples
 
 #Set up installs
-install npm version
-install node verison
-install nvm
+install npm version 6.14.7
+install node verison 12.18.4
+install nvm 1.1.7
 npm install -g @angular/cli
 
 #Run Local
-npm start
+npm install
+npm run start
 
 go to localhost:4200 in broswer
